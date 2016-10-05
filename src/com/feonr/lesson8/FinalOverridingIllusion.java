88f42d0ec24ece4f476c0eacc97c85a9017cbc2d
feonrr <feonrr@gmail.com>
2016-10-05 13:31:58 +0200

    Initial Comit

---
+++ src/com/feonr/lesson8/FinalOverridingIllusion.java
@@ -0,0 +1,9 @@
+package com.feonr.lesson8;
+import static net.feonr.util.Print.*;
+
+
+    public class FinalOverridingIllusion {
+        public static void main(String[] args) {
+
+        }
+    }
--- src/com/feonr/lesson8/Finall.java
+++ src/com/feonr/lesson8/Finall.java
@@ -1,4 +1,21 @@
 package com.feonr.lesson8;
 
+import java.util.Random;
+
 public class Finall {
+    public static Random random=new Random(12);
+    public static final int first=random.nextInt();
+    public final int second=random.nextInt();
+
+    @Override
+    public String toString() {
+        return "first "+first+" second "+second;
+    }
+
+    public static void main(String[] args) {
+        Finall t1=new Finall();
+        Finall t2=new Finall();
+        System.out.println(t1);
+        System.out.println(t2);
+    }
 }
